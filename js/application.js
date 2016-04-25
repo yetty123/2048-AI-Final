9f67fd82f410b281d6b1544d76ab2d217647a506
SpencerFranklin <franklin.sp@husky.neu.edu>
2016-04-25 19:34:36 -0400

    Update application.js

--- js/application.js
+++ js/application.js
@@ -1,5 +1,5 @@
-animationDelay = 100;
 searchDepth = 150;
+animationDelay = 100;
 
 // Wait till the browser is ready to render the game (avoids glitches)
 window.requestAnimationFrame(function () {
