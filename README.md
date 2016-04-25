5e4ad7ae59f0cc6ccb1ec0b5b7ff69121235e226
SpencerFranklin <franklin.sp@husky.neu.edu>
2016-04-25 15:40:00 -0400

    Added files via upload

--- README.md
+++ README.md
@@ -1,3 +1,2 @@
-# 2048-AI-Final
-An implementation of a 2048 AI that uses alpha-beta pruning on minimax and iterative deepening
-//
+# 2048 AI
+
---
+++ js/ai.js
@@ -0,0 +1,148 @@
+function AI(grid) {
+  this.grid = grid;
+}
+
+// calls iterative deepening to find the best move and returns it
+AI.prototype.getBestMove = function() {
+  return this.iterativeDeep();
+}
+
+// performs iterative deepening
+AI.prototype.iterativeDeep = function() {
+  var start = (new Date()).getTime();
+  var depth = 0;
+  var best;
+  do {
+    var newBest = this.search(depth, Number.NEGATIVE_INFINITY, Number.POSITIVE_INFINITY, 0 ,0);
+    if (newBest.move == -1) {
+      break;
+    } else {
+      best = newBest;
+    }
+    depth++;
+  } while ( (new Date()).getTime() - start < searchDepth);
+  return best
+}
+
+// alpha-beta depth first search
+AI.prototype.search = function(depth, alpha, beta, positions, cutoffs) {
+  var bestScore;
+  var bestMove = -1;
+  var result;
+
+  // the maxing player
+  if (this.grid.playerTurn) {
+    bestScore = alpha;
+    for (var direction in [0, 1, 2, 3]) {
+      var newGrid = this.grid.clone();
+      if (newGrid.move(direction).moved) {
+        positions++;
+        if (newGrid.isWin()) {
+          return { move: direction, score: 10000, positions: positions, cutoffs: cutoffs };
+        }
+        var newAI = new AI(newGrid);
+
+        if (depth == 0) {
+          result = { move: direction, score: newAI.heuristic() };
+        } else {
+          result = newAI.search(depth-1, bestScore, beta, positions, cutoffs);
+          if (result.score > 9900) { // win
+            result.score--; // to slightly penalize higher depth from win
+          }
+          positions = result.positions;
+          cutoffs = result.cutoffs;
+        }
+
+        if (result.score > bestScore) {
+          bestScore = result.score;
+          bestMove = direction;
+        }
+        if (bestScore > beta) {
+          cutoffs++
+          return { move: bestMove, score: beta, positions: positions, cutoffs: cutoffs };
+        }
+      }
+    }
+  }
+
+  // minimizing player (computer)
+  else { 
+    bestScore = beta;
+
+    // try a 2 and 4 in each cell
+    // measure heuristics
+    var candidates = [];
+    var cells = this.grid.availableCells();
+    var scores = { 2: [], 4: [] };
+    for (var value in scores) {
+      for (var i in cells) {
+        scores[value].push(null);
+        var cell = cells[i];
+        var tile = new Tile(cell, parseInt(value, 10));
+        this.grid.insertTile(tile);
+        scores[value][i] = -this.grid.smoothness() + this.grid.islands();
+        this.grid.removeTile(cell);
+      }
+    }
+
+    // picks strongest move for computer
+    var maxScore = Math.max(Math.max.apply(null, scores[2]), Math.max.apply(null, scores[4]));
+    for (var value in scores) { // 2 and 4
+      for (var i=0; i<scores[value].length; i++) {
+        if (scores[value][i] == maxScore) {
+          candidates.push( { position: cells[i], value: parseInt(value, 10) } );
+        }
+      }
+    }
+
+    // search on each candidate
+    for (var i=0; i<candidates.length; i++) {
+      var position = candidates[i].position;
+      var value = candidates[i].value;
+      var newGrid = this.grid.clone();
+      var tile = new Tile(position, value);
+      newGrid.insertTile(tile);
+      newGrid.playerTurn = true;
+      positions++;
+      newAI = new AI(newGrid);
+      result = newAI.search(depth, alpha, bestScore, positions, cutoffs);
+      positions = result.positions;
+      cutoffs = result.cutoffs;
+
+      if (result.score < bestScore) {
+        bestScore = result.score;
+      }
+      if (bestScore < alpha) {
+        cutoffs++;
+        return { move: null, score: alpha, positions: positions, cutoffs: cutoffs };
+      }
+    }
+  }
+
+  return { move: bestMove, score: bestScore, positions: positions, cutoffs: cutoffs };
+}
+
+// calculates the heuristic value
+AI.prototype.heuristic = function() {
+  var emptyCells = this.grid.availableCells().length;
+  var smoothness_ = 0.5;
+  var monotonicity_  = 1.0;
+  var freeTile_  = 3.0;
+  
+  return this.grid.smoothness() * smoothness_
+          + this.grid.monotonicity() * monotonicity_
+            + Math.log(emptyCells) * freeTile_
+              + this.grid.maxValue();
+};
+
+
+
+
+AI.prototype.translate = function(move) {
+ return {
+    0: 'up',
+    1: 'right',
+    2: 'down',
+    3: 'left'
+  }[move];
+}
